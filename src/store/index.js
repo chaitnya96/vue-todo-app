c1e3add495cc69d85d7dc40ace762c37aaa33ab1
Chaitnya <chaitnyaparikh@gmail.com>
2021-12-13 02:45:31 +0530

    API call learn with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,10 +6,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: []
   },
   mutations: {
+    SET_TODOS(state, todos) {
+      state.todos = todos
+    }
   },
   actions: {
+    getTodos({ commit }) {
+      axios.get('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=51')
+        .then(response => {
+          commit('SET_TODOS', response.data)
+        })
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,39 @@
 <template>
-  <hello-world />
+    <v-container class="grey lighten-5">
+      <v-row>
+        <v-col v-for="todo in todos" :key="todo.id" cols="12" sm="4">
+          <v-card class="mx-auto pt-4" max-width="400" tile>
+            <v-avatar class="d-flex ma-auto " color="primary" size="20">
+              <img  :src="todo.thumbnailUrl" alt="John" />
+            </v-avatar>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title>{{ todo.title }}</v-list-item-title>
+                <v-list-item-subtitle>
+                  Secondary line text Lorem ipsum dolor sit amet,
+                </v-list-item-subtitle>
+                <v-list-item-subtitle>
+                  consectetur adipiscing elit.
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
+export default {
+  name: "Home",
+  computed: {
+    todos() {
+      return this.$store.state.todos;
     },
-  }
+  },
+  mounted() {
+    this.$store.dispatch("getTodos");
+  },
+};
 </script>
